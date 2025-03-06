cd23aba341b3354b6f52096bbf989ad24ebff7f5
Mateo <monorymateo1@gmail.com>
2025-03-06 17:48:06 -0500

    listo

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function descargarContacto() {
     const vcardData = `BEGIN:VCARD
 VERSION:3.0
-FN:Mariana Buraye
-TEL: +57 3004440392
-NOTE: Bura Concept
+N:Valencia Zuluaga;Dianny;;;
+FN:Dianny Valencia Zuluaga
+TEL:+57 3164829056
+EMAIL;TYPE=WORK: dianny.valencia@autopacifico.com.co
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
